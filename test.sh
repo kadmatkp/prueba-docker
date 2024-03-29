f05c274edc16ee781d755e56ad27876b88aeb75f
kadmatkp <165349020+kadmatkp@users.noreply.github.com>
2024-03-28 21:41:50 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-rclone rcd --rc-web-gui --rc-addr :5572 --rc-serve --rc-user=$USER_RCLONE --rc-pass=$PASSWORD_RCLONE --rc-no-auth --config "/root/.config/rclone/rclone.conf" --user-agent "Mozilla" --transfers 24
+rclone rcd --rc-web-gui --rc-addr :80 --rc-serve --rc-user=$USER_RCLONE --rc-pass=$PASSWORD_RCLONE --rc-no-auth --config "/root/.config/rclone/rclone.conf" --user-agent "Mozilla" --transfers 24
